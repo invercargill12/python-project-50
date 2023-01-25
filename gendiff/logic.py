59e3eba18e1bcba8d14f237dd4c5b96d8f7da8ea
Ivan Noev <ivnoyev@yandex.ru>
2023-01-26 02:22:34 +0600

    fixed formatter work

--- gendiff/logic.py
+++ gendiff/logic.py
@@ -1,9 +1,10 @@
 from gendiff.data_reader import read_data
 from gendiff.diff_tree import generate_tree
+from gendiff.stylish import stylish_message
 
 
 def generate_diff(file1, file2, format='stylish'):
     data1 = read_data(file1)
     data2 = read_data(file2)
-    diff = generate_tree(data1, data2)
+    diff = stylish_message(generate_tree(data1, data2))
     return diff
