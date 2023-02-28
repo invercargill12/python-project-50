6b65baf1c2ec87fbc1aa3027bbe70d5195a12629
Ivan Noev <ivnoyev@yandex.ru>
2023-02-28 12:11:21 +0600

    added default mode in cli.py argparser

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -8,6 +8,7 @@ def arguments_parser(argv=None):
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format',
-                        help='set format of output')
+                        help='set format of output',
+                        default='stylish')
     args = parser.parse_args(argv)
     return args.first_file, args.second_file, args.format
