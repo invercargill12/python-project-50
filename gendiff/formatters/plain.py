fcecff523446b058173543b6633a43760922663a
Ivan Noev <ivnoyev@yandex.ru>
2023-03-04 15:07:00 +0600

    working version of plain formatter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,42 +1,35 @@
-def normalize(value, depth):
+def normalize(value):
     if isinstance(value, dict):
-        return plain(value, path)
+        return '[complex value]'
     elif isinstance(value, bool):
         return str(value).lower()
     elif value is None:
         return 'null'
-    return value          # str?
+    return f"'{str(value)}'"
 
 
-def plain(diff_tree, path=''):  # noqa C901
+def plain(diff_tree, path=''):
     lines = []
 
     for key, description in diff_tree.items():
-        # value determining block
-        if isinstance(description, dict):
-            value = '[complex value]'
-        else:
-            value = description
+        value = description.get('value')
+        info = description.get('info')
 
-        # info determining block
-        if isinstance(description, dict):
-            info = description.get('info')
-        else:
-            info = ''         # проверить что будет если убрать
-
-        # main block
         if info == 'added':
+            value = normalize(value)
             lines.append(
-                f"Property '{path}.{key}' was added with value: '{value}'"
+                f"Property '{path + key}' was added with value: {value}"
             )
         elif info == 'deleted':
             lines.append(
-                f"Property '{path}.{key}' was removed"
+                f"Property '{path + key}' was removed"
             )
         elif info == 'changed':
-            value1, value2 = value
+            value1, value2 = map(normalize, value)
             lines.append(
-                f"Property '{path}.{key}' was updated. From '{value1}' to '{value2}'"  # noqa E501
+                f"Property '{path + key}' was updated. From {value1} to {value2}"  # noqa E501
             )
+        elif isinstance(value, dict):
+            lines.append(plain(value, path + key + '.'))
 
     return '\n'.join(lines)
