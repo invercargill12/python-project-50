9400bad1f477eecaec8814cdb657982d82415a48
Ivan Noev <ivnoyev@yandex.ru>
2023-03-17 00:46:27 +0600

    import error fixes

--- tests/test_argparser.py
+++ tests/test_argparser.py
@@ -1,21 +1,21 @@
-from gendiff.cli import arguments_parser
+from gendiff.cli import run_argparse
 
 
 def test_argparser_json():
-    paths = arguments_parser(['tests/fixtures/file1.json', 'tests/fixtures/file2.json'])  # noqa E501
+    paths = run_argparse(['tests/fixtures/file1.json', 'tests/fixtures/file2.json'])  # noqa E501
     assert paths == ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'stylish')  # noqa E501
 
 
 def test_argparser_json_nested():
-    paths = arguments_parser(['tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json'])  # noqa E501
+    paths = run_argparse(['tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json'])  # noqa E501
     assert paths == ('tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json', 'stylish')  # noqa E501
 
 
 def test_argparser_yml():
-    paths = arguments_parser(['tests/fixtures/file1.yml', 'tests/fixtures/file2.yml'])  # noqa E501
+    paths = run_argparse(['tests/fixtures/file1.yml', 'tests/fixtures/file2.yml'])  # noqa E501
     assert paths == ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'stylish')  # noqa E501
 
 
 def test_argparser_yml_nested():
-    paths = arguments_parser(['tests/fixtures/file1_nested.yml', 'tests/fixtures/file2_nested.yml'])  # noqa E501  
+    paths = run_argparse(['tests/fixtures/file1_nested.yml', 'tests/fixtures/file2_nested.yml'])  # noqa E501  
     assert paths == ('tests/fixtures/file1_nested.yml', 'tests/fixtures/file2_nested.yml', 'stylish')  # noqa E501
